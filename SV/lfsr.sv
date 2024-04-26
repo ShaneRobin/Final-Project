03e5935510164c68b41d595ed980c3923518f691
JoelxCode <nnajijoel@gmail.com>
2024-04-26 13:18:22 -0500

    LFSR done. LFSR TB almost done
    We are working on the LFSR testbench. Currently, we are trying to complete building the output to the lfsr_out.out file.
    
    We are getting some output errors but we can check the lab2 output format to see what we are doing incorrectly.

--- SV/lfsr.sv
+++ SV/lfsr.sv
@@ -5,8 +5,23 @@ module lfsr(seed, clk, reset, shift_seed);
 //your implementation of the LFSR.  Remember that this 
 //implementation has memory so it should be done 
 //with some form of a flip-flop based register
+input logic seed; 
+input logic clk; 
+input logic reset;
+input logic [15:0]shift_seed;
+
+always @(posedge clk) begin
+
+    if(reset) begin
+    shift_seed = seed;
+    end
+    else begin
+    shift_seed = {shift_seed[14:0], shift_seed[15]^shift_seed[12]^shift_seed[3]};
+    end
+end
 
 endmodule
+
 
 module lfsr64 (seed, clk, reset, shift_seed);
 //inputs and outputs for the full seed size (64 bits)
@@ -14,5 +29,19 @@ module lfsr64 (seed, clk, reset, shift_seed);
 //either build a 64 bit version using your smaller implementation above
 //or use the same methods from the xilinx document to build a full
 //64 bit version
+input logic seed; 
+input logic clk; 
+input logic reset;
+output logic [63:0]shift_seed;
+
+always @ (posedge clk) begin
+
+    if (reset) begin
+    shift_seed = seed;
+    end
+    else begin
+    shift_seed = {shift_seed[63:0], shift_seed[63]^shift_seed[62]^shift_seed[60]^shift_seed[59]};
+    end
+end
 
 endmodule
--- SV/lfsr_tb.sv
+++ SV/lfsr_tb.sv
@@ -1,31 +1,34 @@
 // testbench to prove maximal LFSR
-module tb ();
 
    //logic variables to route input and output to DUT
-
+module tb (seed, clk, reset, shift_seed);
+  input logic seed;
+  input logic clk;
+  input logic reset;
+  output logic shift_seed;
+  integer handle3;
+  integer desc3;
    //create file handles to write results to a file
-   
-   // instantiate device under test (small LFSR)
 
+   // instantiate device under test (small LFSR)
+  lfsr band(seed, clk, reset, shift_seed);
    //set up a clock signal
    always     
      begin
 	clk = 1; #1; clk = 0; #1;
      end
-   
    initial
      begin
-	//set up output file
-	//set up any book keeping variables you may want to use
-	//set up a starting seed.  What happens with all 0s?
-	//reset your DUT
-	//save the initial output of your DUT to compare with current output
-	//and see whenb you repeat
+         handle3 = $fopen("lfsr_out.out");
+         desc3 = handle3;
      end
 
    always @(posedge clk)
      begin
-		//output your results to a file
+		//output your results to a file 
+     $fdisplay (desc3, "%h %h || %b "
+      seed, shift_seed, reset,)
+
      end
 
    // check results on falling edge of clk
